d370065c3421383d96b359f60060e98ef44b52c1
quangnd <dinhquang0527@gmail.com>
2026-06-24 19:56:00 +0700

    Quản lý phòng và loại phòng

--- backend/config/db.js
+++ backend/config/db.js
@@ -5,7 +5,7 @@ const db = mysql.createPool({
   host: process.env.DB_HOST || 'localhost',
   user: process.env.DB_USER || 'root',
   password: process.env.DB_PASSWORD || '',
-  database: process.env.DB_NAME || 'hotelbookingdb',
+  database: process.env.DB_NAME || 'datn',
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
