89314d3c302cc5ab2ede20c52b43d77b2dffd0c2
quangnd <dinhquang0527@gmail.com>
2026-08-16 13:02:49 +0700

    3.  Chuẩn hóa Tùy chọn Độ tuổi Trẻ em

--- src/components/SearchForm/SearchForm.tsx
+++ src/components/SearchForm/SearchForm.tsx
@@ -177,11 +177,9 @@ const SearchForm: React.FC = () => {
                   onChange={(val) => handleAgeChange(index, val)}
                   size="small"
                 >
-                  {Array.from({ length: 18 }, (_, i) => (
-                    <Option key={i} value={i}>
-                      {i === 0 ? 'Dưới 1 tuổi' : `${i} tuổi`}
-                    </Option>
-                  ))}
+                  <Option value={4}>Dưới 5 tuổi (Miễn phí)</Option>
+                  <Option value={8}>Từ 6 - 11 tuổi (Phụ thu trẻ em)</Option>
+                  <Option value={12}>Trên 11 tuổi (Tính người lớn)</Option>
                 </Select>
               </div>
             ))}
